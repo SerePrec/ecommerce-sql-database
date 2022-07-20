a2036e2e0050bb0644917a26d0cd7b41791c53e0
Sergio E. Prellezo <sereprec@gmail.com>
2022-07-20 00:03:48 -0300

    Users

--- scripts/mammoth-DB.sql
+++ scripts/mammoth-DB.sql
@@ -3696,3 +3696,38 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+/*************************************************
+	     CREACIÓN DE USUARIOS Y PERMISOS
+ *************************************************/
+
+# USER: reader
+# ************
+
+# Creo el usuario reader con su contraseña
+CREATE USER reader@localhost IDENTIFIED BY 'Reader2022';
+
+# verifico su correcta creación en la tabla users de la BD mysql
+-- SELECT * FROM mysql.user;
+
+# Le doy permisos sólo de lectura al usuario reader sobre todas las tablas y vistas de la base de datos mammoth
+GRANT SELECT ON mammoth.* TO reader@localhost;
+
+# verifico los permisos del usuario reader
+-- SHOW GRANTS FOR reader@localhost;
+
+# USER: writer
+# ************
+
+# Creo el usuario writer con su contraseña
+CREATE USER writer@localhost IDENTIFIED BY 'Writer2022';
+
+# verifico su correcta creación en la tabla users de la BD mysql
+-- SELECT * FROM mysql.user;
+
+# Le doy permisos de lectura, inserción y actualización al usuario writer sobre todas las tablas y vistas de la base de datos mammoth
+GRANT SELECT, INSERT, UPDATE ON mammoth.* TO writer@localhost;
+
+# verifico los permisos del usuario writer
+-- SHOW GRANTS FOR writer@localhost;
+
